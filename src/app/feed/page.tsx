d8007dfda587ae1caa71143ecb7de141f061e91a
judahwon <hwondaa@gmail.com>
2024-12-05 21:21:37 +0900

    fix: feed 생성방식 변경

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,18 +1,16 @@
-// app/feed/page.tsx
+// src/app/feed/page.tsx
 
-import { GetStaticProps } from 'next';
-import RSS from 'rss';
-import { fetchTermsData } from '@/utils/termsData'; // 데이터를 가져오는 함수
-import { TermData } from '@/types/database'; // 데이터 타입
+import { fetchTermsData } from '@/utils/termsData'; // 데이터 가져오는 함수
 import { dikiMetadata } from '@/constants';
-import fs from 'fs';
-import path from 'path';
+import RSS from 'rss';
+import { TermData } from '@/types/database';
 
-// getStaticProps를 사용하여 빌드 타임에 RSS 파일을 생성합니다.
-export const getStaticProps: GetStaticProps = async () => {
+export default async function FeedPage() {
+  // 포스트 데이터를 서버에서 직접 가져옵니다.
   const postLists: TermData[] = await fetchTermsData();
   const metadata = dikiMetadata;
 
+  // RSS 객체 생성
   const feed = new RSS({
     title: '데이터 위키',
     description: '데이터 위키, 데이터 백과사전 - 디키',
@@ -24,36 +22,27 @@ export const getStaticProps: GetStaticProps = async () => {
     ttl: 60, // 60분마다 업데이트
   });
 
+  // 포스트들을 RSS 항목으로 추가
   postLists.forEach((post) => {
     feed.item({
-      title: post.title.ko,
-      description: post.description.short,
-      url: metadata.url + post.url,
-      guid: post.url,
-      categories: post.usecase.industries,
-      author: post.metadata.authors.join(', '),
-      date: new Date(post.metadata.updated_at),
+      title: post.title.ko, // 한국어 제목
+      description: post.description.short, // 짧은 설명
+      url: metadata.url + post.url, // 포스트 URL
+      guid: post.url, // GUID (URL)
+      categories: post.usecase.industries, // 산업 카테고리
+      author: post.metadata.authors.join(', '), // 저자
+      date: new Date(post.metadata.updated_at), // 업데이트 날짜
     });
   });
 
+  // XML 문자열 생성
   const xml = feed.xml({ indent: true });
 
-  // 'public/feed.xml' 파일에 저장
-  const filePath = path.join(process.cwd(), 'public', 'feed.xml');
-  fs.writeFileSync(filePath, xml);
-
-  return {
-    props: {},
-  };
-};
-
-const FeedPage = () => {
+  // RSS XML을 반환합니다.
   return (
     <div>
       <h1>{'RSS Feed'}</h1>
-      <p>{'RSS Feed가 정상적으로 생성되었습니다. '}<a href="/feed.xml">{'XML 파일 보기'}</a></p>
+      <pre>{xml}</pre>
     </div>
   );
-};
-
-export default FeedPage;
+}
