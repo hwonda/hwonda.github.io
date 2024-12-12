b31168d1090b7937c1708a85614cb31e28219016
judahwon <hwondaa@gmail.com>
2024-12-12 21:38:00 +0900

    fix: thumbnail url 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
 export const dikiMetadata = {
   title: '디키',
   description: '데이터 백과사전 - 디키(Diki)',
-  url: 'https://dxwiki.github.io',
-  thumbnailURL: 'https://www.dxwiki.github.io/thumbnail.png',
+  url: 'https://hwonda.github.io',
+  thumbnailURL: 'https://www.hwonda.github.io/thumbnail.png',
   author: {
     name: '윤대희, 주다훤',
   },
