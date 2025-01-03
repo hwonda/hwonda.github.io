a4fecf8daa644a953ed09b5ff2dfcdf422dae4c0
judahwon <hwondaa@gmail.com>
2025-01-03 18:33:44 +0900

    fix: next config

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   ...(isProd && {
     output: 'export', // 배포 환경에서 정적 사이트 생성
-    basePath: '/hwonda.github.io', // GitHub Pages의 리포지토리 이름으로 변경
-    assetPrefix: '/hwonda.github.io', // 정적 파일 경로
   }),
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push(
