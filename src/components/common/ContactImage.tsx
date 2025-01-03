8401313dedaf53ac05b9092631d700deba1e4f8e
judahwon <hwondaa@gmail.com>
2025-01-03 18:37:07 +0900

    fix: image path

--- src/components/common/ContactImage.tsx
+++ src/components/common/ContactImage.tsx
@@ -8,6 +8,8 @@ const ContactImage = () => {
   return (
     <>
       <Image src="/images/github-mark.png" alt='절대경로' width={100} height={100} />
+      <Image src="/public/images/github-mark.png" alt='절대경로' width={100} height={100} />
+      <Image src="https://hwonda.github.io/images/github-mark.png" alt='절대경로' width={100} height={100} />
       <Image src={contact1} alt='상대-public' width={100} height={100} />
       <Image src={contact2} alt='상대-assets' width={100} height={100} />
       <Image src={contact3} alt='상대-assets git' width={100} height={100} />
