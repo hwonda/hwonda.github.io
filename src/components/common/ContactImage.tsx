56072568bd6474640afcc4c0a3654a41024da5b6
judahwon <hwondaa@gmail.com>
2025-01-03 18:41:09 +0900

    fix: Image->img

--- src/components/common/ContactImage.tsx
+++ src/components/common/ContactImage.tsx
@@ -1,18 +1,10 @@
-import Image from 'next/image';
-
-import contact1 from '../../../public/images/github-mark.png';
-import contact2 from '@/assets/images/linkedin.jpeg';
-import contact3 from '@/assets/images/github-mark.png';
 
 const ContactImage = () => {
   return (
     <>
-      <Image src="/images/github-mark.png" alt='절대경로' width={100} height={100} />
-      <Image src="/public/images/github-mark.png" alt='절대경로' width={100} height={100} />
-      <Image src="https://hwonda.github.io/images/github-mark.png" alt='절대경로' width={100} height={100} />
-      <Image src={contact1} alt='상대-public' width={100} height={100} />
-      <Image src={contact2} alt='상대-assets' width={100} height={100} />
-      <Image src={contact3} alt='상대-assets git' width={100} height={100} />
+      <img src="/images/github-mark.png" alt='절대경로' className='size-10  ' />
+      <img src="/public/images/github-mark.png" alt='절대경로' className='size-10' />
+      <img src="https://hwonda.github.io/images/github-mark.png" alt='절대경로' className='size-10' />
     </>
   );
 };
