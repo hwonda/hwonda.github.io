26a162c7e968915b301c98e4096fe3de6527fbf9
judahwon <hwondaa@gmail.com>
2025-02-20 15:20:10 +0900

    fix: post.tilte.etc 피드 수정

--- scripts/generateFeed.ts
+++ scripts/generateFeed.ts
@@ -9,7 +9,7 @@ const generateItemXML = (post: TermData, metadata: typeof dikiMetadata): string 
 
   return `
     <item>
-      <title>${ sanitizeText(post.title?.ko ?? '') }${ sanitizeText(post.title?.en ?? '') }${ sanitizeText(post.title?.etc ?? '') }</title>
+      <title>${ sanitizeText(post.title?.ko ?? '') }${ sanitizeText(post.title?.en ?? '') }${ sanitizeText(post.title?.etc?.[0] ?? '') }</title>
       <description><![CDATA[${ sanitizeText(description) }]]></description>
       <link>${ metadata.url }${ post.url }</link>
       <guid>${ metadata.url }${ post.url }</guid>
