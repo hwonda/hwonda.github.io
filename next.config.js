32a6c228247629a1a9180caad506052232b0b2c4
judahwon <hwondaa@gmail.com>
2025-02-27 08:33:02 +0900

    fix: 캐시 제거, next config 변경

--- next.config.js
+++ next.config.js
@@ -1,21 +1,35 @@
+// /** @type {import('next').NextConfig} */
+
+// const isProd = process.env.NODE_ENV === 'production';
+
+// const nextConfig = {
+//   output: 'standalone',
+//   images: {
+//     unoptimized: true,
+//   },
+//   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//     config.module.rules.push(
+//       {
+//         test: /\.(frag|vert)$/,
+//         // Asset module for shader files
+//         type: 'asset/source',
+//       }
+//     );
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
+  },
   images: {
     unoptimized: true,
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push(
-      {
-        test: /\.(frag|vert)$/,
-        // Asset module for shader files
-        type: 'asset/source',
-      }
-    );
-    return config;
   },
 };
 
