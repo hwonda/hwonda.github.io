97c4419d0cfc9446658000c83be20d427ed7ae46
judahwon <hwondaa@gmail.com>
2025-02-28 19:29:46 +0900

    fix: sitemap temp3, temp4 추가

--- scripts/generateSitemap.ts
+++ scripts/generateSitemap.ts
@@ -1,6 +1,5 @@
 import { writeFileSync } from 'fs';
 import { generateSitemapXML } from '../src/utils/sitemap';
-import { dikiMetadata } from '../src/constants';
 
 (async () => {
   // 기존 전체 사이트맵 생성
@@ -9,14 +8,13 @@ import { dikiMetadata } from '../src/constants';
   const sitemap = generateSitemapXML(urls);
 
   // 간소화된 사이트맵용 URL (PostList 제외)
-  const baseUrl = dikiMetadata.url;
   const formatDate = (date: Date): string => {
     return date.toISOString().split('T')[0] + 'T00:00:00+00:00';
   };
 
   const simplifiedUrlsTemp = [
     {
-      loc: baseUrl,
+      loc: 'https://hwonda.github.io',
       lastmod: formatDate(new Date()),
       changefreq: 'always',
       priority: 1.00,
@@ -25,16 +23,10 @@ import { dikiMetadata } from '../src/constants';
 
   const simplifiedUrlsTemp2 = [
     {
-      loc: baseUrl,
+      loc: 'https://hwonda.github.io/',
       lastmod: formatDate(new Date()),
       changefreq: 'always',
       priority: 1.00,
-    },
-    {
-      loc: `${ baseUrl }/posts`,
-      lastmod: formatDate(new Date()),
-      changefreq: 'always',
-      priority: 0.80,
     },
   ];
 
@@ -44,8 +36,8 @@ import { dikiMetadata } from '../src/constants';
   // 사이트맵 파일 생성
   await Promise.all([
     writeFileSync('out/sitemap.xml', sitemap, 'utf-8'),
-    writeFileSync('out/sitemap-temp.xml', simplifiedSitemap, 'utf-8'),
-    writeFileSync('out/sitemap-temp2.xml', simplifiedSitemap2, 'utf-8'),
+    writeFileSync('out/sitemap-temp3.xml', simplifiedSitemap, 'utf-8'),
+    writeFileSync('out/sitemap-temp4.xml', simplifiedSitemap2, 'utf-8'),
     // writeFileSync('public/sitemap.xml', sitemap, 'utf-8'),
     // writeFileSync('public/sitemap-temp.xml', simplifiedSitemap, 'utf-8'),
     // writeFileSync('public/sitemap-temp2.xml', simplifiedSitemap2, 'utf-8'),
